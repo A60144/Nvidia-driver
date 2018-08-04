dbbbe97abeba1e06be972328c15884b1ba3e59a8
TheresaLiao <33051006+A60144@users.noreply.github.com>
2018-08-04 13:04:27 +0800

    Create install_nvidia-docker2_centos7.sh

---
+++ install_nvidia-docker2_centos7.sh
@@ -0,0 +1,7 @@
+docker volume ls -q -f driver=nvidia-docker | xargs -r -I{} -n1 docker ps -q -a -f volume={} | xargs -r docker rm -f
+sudo yum remove nvidia-docker
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.repo | sudo tee /etc/yum.repos.d/nvidia-docker.repo
+sudo yum install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+docker run --runtime=nvidia --rm nvidia/cuda nvidia-smi
