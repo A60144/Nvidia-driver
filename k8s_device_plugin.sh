11d23db5c9aafb7607af17460c6df93daaca8130
TheresaLiao <33051006+A60144@users.noreply.github.com>
2018-08-04 15:43:34 +0800

    Update k8s_device_plugin.sh

--- k8s_device_plugin.sh
+++ k8s_device_plugin.sh
@@ -1,3 +1,4 @@
 echo "Environment=\"KUBELET_EXTRA_ARGS=--feature-gates=DevicePlugins=true\"">> /etc/systemd/system/kubelet.service.d/10-kubelet.conf
 sudo systemctl daemon-reload
 sudo systemctl restart kubelet
+kubectl create -f https://raw.githubusercontent.com/NVIDIA/k8s-device-plugin/v1.11/nvidia-device-plugin.yml
