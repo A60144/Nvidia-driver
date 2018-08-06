874c25b6aca88653d6d3df26205d78c00eead4c0
TheresaLiao <33051006+A60144@users.noreply.github.com>
2018-08-06 17:31:02 +0800

    Update install_390.67_centos7.sh

--- install_390.67_centos7.sh
+++ install_390.67_centos7.sh
@@ -7,6 +7,6 @@ yum install dkms -y
 echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 sudo systemctl stop lightdm
 sudo dracut --force
-yum install gcc kernal-devel-$(uname -r)
+yum install gcc kernel-devel-$(uname -r) -y
 sudo ./NVIDIA-Linux-x86_64-384.69.run --dkms -s
 nvidia-smi
