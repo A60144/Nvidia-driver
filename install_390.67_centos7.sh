c9215e8d8b4a8c668293d8b612bedccb5059a944
TheresaLiao <33051006+A60144@users.noreply.github.com>
2018-08-06 17:25:47 +0800

    Update install_390.67_centos7.sh

--- install_390.67_centos7.sh
+++ install_390.67_centos7.sh
@@ -2,8 +2,8 @@ mkdir ~/nvidia_doc
 cd ~/nvidia_doc
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/390.67/NVIDIA-Linux-x86_64-390.67.run
 chmod +x NVIDIA-Linux-x86_64-390.67.run
-yum install epel-release
-yum install dkms
+yum install epel-release -y
+yum install dkms -y
 echo -e "blacklist nouveau\noptions nouveau modeset=0" | sudo tee /etc/modprobe.d/blacklist-nouveau.conf
 sudo systemctl stop lightdm
 sudo dracut --force
